63c7d10147d77264d7ac85486aa62ffeca8d47d1
premshyam <prem.shyam01@gmail.com>
2021-09-07 18:54:44 +0530

    updated nwk_status.sh

--- scripts/nwk_status.sh
+++ scripts/nwk_status.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+ap_mode=false
 while :
 do
-	#get configured SSID 
-	SSID=$(wpa_cli -i wlan0 get_network 0 ssid)
-	#check if connected to AP
-	OUTPUT=$(iwconfig wlan0 | grep -c $SSID)
-	if [ "$OUTPUT" = "0" ]
+	if [ $(hostname -I) == "192.168.4.1" ]
 	then
-		echo "Not associated to AP"
-		/home/pi/start_ap.sh
-	else
-		echo "Associate to AP"
+		ap_mode=true
+		echo "Device hotspot enabled"
 	fi
-	sleep 5
+	if [ $ap_mode == false ]
+	then
+		#get configured SSID 
+		SSID=$(wpa_cli -i wlan0 get_network 0 ssid)
+		#check if connected to AP
+		OUTPUT=$(iwconfig wlan0 | grep -c "$SSID")
+		if [ "$OUTPUT" == "0" ]
+		then
+			echo "Not associated to AP"
+			/home/pi/start_ap.sh
+			ap_mode=true
+		else
+			echo "Associate to AP"
+		fi
+	fi
+	sleep 10
 done
